d0a0f4ff7d4c59eff1089d590bfb97828f93772b
bortunac <bortunadrian@gmail.com>
2020-02-03 17:05:41 +0200

    qr_code added

--- www/js/index.js
+++ www/js/index.js
@@ -52,10 +52,92 @@ var app = {
         var nf = function () { };
         document.getElementById("but1").addEventListener("click", ev => {
             navigator.vibrate(100);
-            alert("but1");
+            navigator.notification.prompt("esti de acord?", function (rez) {
+                if (rez.buttonIndex === 1) {
+                    alert("ai fost de acord!", "DECI ASA!");
+                }
+                else {
+                    navigator.notification.beep(1);
+                }
+            });
         });
+
+        function setOptions(srcType) {
+            var options = {
+                // Some common settings are 20, 50, and 100
+                quality: 50,
+                destinationType: Camera.DestinationType.FILE_URI,
+                // In this app, dynamically set the picture source, Camera or photo gallery
+                sourceType: srcType,
+                //encodingType: Camera.EncodingType.JPEG,
+                encodingType: Camera.EncodingType.PNG,
+                mediaType: Camera.MediaType.PICTURE,
+                _allowEdit: true,
+                correctOrientation: true  //Corrects Android orientation quirks
+            }
+            return options;
+        }
+
+        function openCamera(selection) {
+            
+
+            var srcType = Camera.PictureSourceType.CAMERA;
+            var options = setOptions(srcType);
+            //var func = createNewFileEntry;
+            console.log("333")
+            if (selection == "camera-thmb") {
+                options.targetHeight = 100;
+                options.targetWidth = 100;
+            }
+            navigator.camera.getPicture(function cameraSuccess(imageUri) {
+                console.log(imageUri);
+                displayImage(imageUri);
+                // You may choose to copy the picture, save it somewhere, or upload.
+                //func(imageUri);
+
+            }, function cameraError(error) {
+                console.debug("Unable to obtain picture: " + error, "app");
+
+            }, options);
+        }
+
+        function displayImage(imgUri) {
+
+            var elem = document.getElementById('myImage');
+            elem.src = imgUri;
+        }
+
+        
+
+function scan_qr(){
+    cordova.plugins.barcodeScanner.scan(
+        function (result) {
+            alert("We got a barcode\n" +
+                  "Result: " + result.text + "\n" +
+                  "Format: " + result.format + "\n" +
+                  "Cancelled: " + result.cancelled);
+        },
+        function (error) {
+            alert("Scanning failed: " + error);
+        },
+        {
+            _preferFrontCamera : true, // iOS and Android
+            showFlipCameraButton : true, // iOS and Android
+            showTorchButton : true, // iOS and Android
+            torchOn: true, // Android, launch with the torch switched on (if available)
+            saveHistory: true, // Android, save scan history (default false)
+            prompt : "Place a barcode inside the scan area", // Android
+            resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+            formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+            orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+            disableAnimations : true, // iOS
+            disableSuccessBeep: false // iOS and Android
+        }
+     );
+}
         document.getElementById("but2").addEventListener("click", ev => {
-            alert("but2");
+            scan_qr()
+            //openCamera("_camera-thmb");
         });
     }
-};
+}
